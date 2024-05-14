586ce98607f5d48ab16996df88fd30493d3b0bae
adfips <arturkhalilov@mail.ru>
2024-05-15 00:04:39 +0500

    добавил javadoc

--- src/main/java/locale/LocalizationListener.java
+++ src/main/java/locale/LocalizationListener.java
@@ -1,5 +1,11 @@
 package locale;
 
+/**
+ * Контракт на который подписываются классы поддерживающие разные локали
+ */
 public interface LocalizationListener {
+    /**
+     * Метод изменения локали
+     */
     void localeChanged();
 }
